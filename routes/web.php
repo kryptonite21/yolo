6671c6ed8603e3a8fb11b1b462d5b8f69a59c27a
Robin <rjcor21@gmail.com>
2019-05-05 13:53:15 +0800

    Added a welcome message to default route.

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@
 */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+    return '<h1>Hello, Welcome to Yolo APIs</h1>';
 });
 
 $router->group(['prefix' => 'api'], function () use ($router) {
